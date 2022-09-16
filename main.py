3f3de7f55198f3f5f5c8ba79348798e6658a9770
jjj kkk <113679198+TheDutchMarof@users.noreply.github.com>
2022-09-16 10:07:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame, sys
+
+class Game:
+  def _init_(self):
+    #general setup
+    pygame.init()
+    self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    self.clock = pygame.time.clock()
+
+  def run(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.quit()
+      self.screen.fill("Black")
+      pygame.display.update()
+      self.clock.tick(FPS)
+
+if __name__ == '__main__':
+  game = Game()
+  game.run
